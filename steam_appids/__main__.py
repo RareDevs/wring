ec444be6421186752d7cb21ecf47b0401a2f87c2
Stelios Tsampas <loathingkernel@gmail.com>
2025-03-13 19:21:55 +0200

    steam_appids: add script

---
+++ steam_appids/__main__.py
@@ -0,0 +1,34 @@
+import os
+
+import orjson
+import requests
+
+steam_api_url = "https://api.steampowered.com/IStoreService/GetAppList/v1/"
+
+
+if __name__ == "__main__":
+    have_more_results = True
+    last_appid = 0
+
+    if (steam_api_key := os.environ.get("STEAM_API_KEY", None)) is None:
+        print("Missing steam api key!")
+        exit(1)
+
+    payload = {
+        "key": steam_api_key,
+        "include_games": True,
+        "max_results": 25000,
+    }
+
+    games = {}
+    while have_more_results:
+        payload.update({"last_appid": last_appid})
+        resp = requests.get(steam_api_url, params=payload)
+        response = orjson.loads(resp.text)["response"]
+        if have_more_results := response.get("have_more_results", False):
+            last_appid = response["last_appid"]
+        for app in response["apps"]:
+            games.update({str(app["appid"]): app["name"]})
+
+    with open("steam_appids.json", "w", encoding="utf-8") as f:
+        f.write(orjson.dumps({"version": 2, "games": games}).decode("utf-8"))
