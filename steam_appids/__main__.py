b79e6858a11861c7c68c008c3a0091c9efd2caea
Stelios Tsampas <loathingkernel@gmail.com>
2025-05-09 11:01:06 +0300

    steam_appids: some titles are missing, no idea what is going on

--- steam_appids/__main__.py
+++ steam_appids/__main__.py
@@ -8,22 +8,23 @@ isteamapps_url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
 istoreservice_url = "https://api.steampowered.com/IStoreService/GetAppList/v1/"
 version = 3
 
+
+def is_valid_title(title: str) -> bool:
+    return bool(title) and len(title) > 2
+
+
 if __name__ == "__main__":
     if (steam_api_key := os.environ.get("STEAM_API_KEY", None)) is None:
         print("Missing steam api key!")
         exit(1)
 
-    def is_valid_title(title: str) -> bool:
-        return bool(title) and len(title) > 2
-
     data = {}
 
     resp = requests.get(isteamapps_url)
     apps = orjson.loads(resp.text).get("applist", {}).get("apps", {})
-    data.update(
-        { app["name"]: str(app["appid"]) for app in apps if is_valid_title(app["name"]) }
-    )
-
+    for app in apps:
+        if is_valid_title(app["name"]):
+            data[app["name"]] = str(app["appid"])
 
     have_more_results = True
     last_appid = 0
@@ -31,7 +32,7 @@ if __name__ == "__main__":
         "key": steam_api_key,
         "include_games": True,
         "include_dlc": False,
-        "max_results": 5000,
+        "max_results": 1000,
     }
 
     while have_more_results:
@@ -41,9 +42,9 @@ if __name__ == "__main__":
         apps = response.get("apps", {})
         if have_more_results := response.get("have_more_results", False):
             last_appid = response["last_appid"]
-        data.update(
-            { app["name"]: str(app["appid"]) for app in apps if is_valid_title(app["name"]) }
-        )
+        for app in apps:
+            if is_valid_title(app["name"]):
+                data[app["name"]] = str(app["appid"])
 
     with open("steam_appids.json", "w", encoding="utf-8") as f:
         f.write(orjson.dumps({"version": version, "games": data}).decode("utf-8"))
